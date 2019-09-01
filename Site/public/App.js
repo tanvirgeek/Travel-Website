0b858401c9ae474f60a471f6ca9e7e9bde0d751a
tanvirgeek <tanvirgeek@gmail.comm>
2019-09-01 16:54:48 +0600

    day three working day ending

---
+++ Site/public/App.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var fortune = require('./lib/fortune.js');
+var app = express();
+
+//Set up Handlebar view Engine
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine','handlebars');
+
+//public
+app.use(express.static(__dirname + '/public'));
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/',function(req, res){
+	res.render('home');
+})
+
+
+app.get('/about', function(req,res){
+	res.render('about', { fortn: fortune.getFortune()});
+});
+
+//custom 404 page
+app.use(function(req,res){
+	res.type('text/plain');
+	res.status(404);
+	res.render('404');
+})
+
+//custom 500 page
+app.use(function(err,req,response,next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('express Started on http localhost:' + app.get('port') + 'press CTRL C to terminate');
+});
